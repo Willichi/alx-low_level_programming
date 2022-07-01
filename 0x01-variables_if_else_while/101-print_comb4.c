fd1e6a6954398f6118c325224529eb82b4c1c161
willichi <onochiewilliam98@gmail.com>
2022-07-01 10:59:21 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,28 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int d;
+int i;
+int j;
+int k;
 
-	for (d = 0; d < 90; d++)
-	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+for (k = '0'; k <= '7'; k++)
+{
+for (i = k + 1; i <= '8'; i++)
+{
+for (j = i + 1; j <= '9'; j++)
+{
+putchar (k);
+putchar(i);
+putchar (j);
+
+if (j < '9' || i < '8' || k < '7')
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
