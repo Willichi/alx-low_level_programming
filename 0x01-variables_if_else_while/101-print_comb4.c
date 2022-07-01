100abb0eb5463b94c7b74660a5e3b20c4d26d56e
willichi <onochiewilliam98@gmail.com>
2022-07-01 11:10:02 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,26 +11,25 @@ int i;
 int j;
 int k;
 
-         for (k = '0'; k <= '7'; k++)
+for (k = '0'; k <= '7'; k++)
 {
-         for (i = k + 1; i <= '8'; i++)
+for (i = k + 1; i <= '8'; i++)
 {
-         for (j = i + 1; j <= '9'; j++)
+for (j = i + 1; j <= '9'; j++)
 {
-          putchar (k);
-          putchar(i);
-          putchar (j);
+putchar (k);
+putchar(i);
+putchar (j);
 
-       if (j < '9' || i < '8' || k < '7')
+if (j < '9' || i < '8' || k < '7')
 {
-            putchar(',');
-            putchar(' ');
+putchar(',');
+putchar(' ');
 
 }
 }
 }
 }
 putchar('\n');
-    return (0);
+return (0);
 }
-
