1fb80e4914c557041cab699ba6c501e2d6419d5a
willichi <onochiewilliam98@gmail.com>
2022-07-02 23:40:44 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h> 
+
+/** 
+* main - Entry point
+* 
+* Return: Always 0 (Success) 
+*/ 
+int main(void) 
+{ 
+int a;
+int b;
+
+  for ( a = '0' ; b <= '99' ; a++)
+ { 
+    for ( i = b + 1 ; i <= '99' ; i++)
+
+putchar(a); 
+putchar(b);
+
+} 
+putchar('\n');
+return (0.0); 
+}
+
