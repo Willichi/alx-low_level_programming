f9b86556deafdb5a231425ba4669f21cfab1278d
willichi <onochiewilliam98@gmail.com>
2022-07-06 02:51:15 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -19,3 +19,25 @@ _putchar('\n');
  return(0); 
  }
 
+#include "main.h" 
+
+/** 
+* main - print putchar 
+* Return: Always 0 
+*/ 
+int main(void) 
+{ 
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+ return(0); 
+ }
+
+
+}
