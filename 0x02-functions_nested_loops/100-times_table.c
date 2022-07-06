8fc34f68d53f8896962a9096c6fb72186bf5dcf8
willichi <onochiewilliam98@gmail.com>
2022-07-06 01:41:05 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,21 @@
 
 /**
 * print_times_table - Entry point
-* @n: input
+*If n is greater than 15 or less than 0 the function should not print anything
 * Return: Always 0 (Success)
 */
 void print_times_table(int n)
 {
-    
+int n;
+n = 15;
+{
+for (n = 0; n <= 15;)
+print_times_table(2);
+_putchar('\n');
+print_times_table(14);
+_putchar('\n');
+print _times_table (25);
+   
+} 
 return (0);
 }
