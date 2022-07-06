9e5c6d558a1a8e2faba46869b4c4b3caa9a8f907
willichi <onochiewilliam98@gmail.com>
2022-07-06 01:44:16 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,12 +5,12 @@
 *If n is greater than 15 or less than 0 the function should not print anything
 * Return: Always 0 (Success)
 */
-void print_times_table(int n)
+void (int n)
 {
-int n;
-n = 15;
+int a;
+a = 15;
 {
-for (n = 0; n <= 15;)
+for (a = 0; a <= 15;)
 print_times_table(2);
 _putchar('\n');
 print_times_table(14);
