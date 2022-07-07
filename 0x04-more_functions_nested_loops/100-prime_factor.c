58c05ea1840ee8ed3a33228a2f7dc384037e5dc6
willichi <onochiewilliam98@gmail.com>
2022-07-07 04:19:10 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+ 
+int main (void) 
+{ 
+long long int number=600851475143, large_factor=0, i=2; 
+while (number!=1)
+{ 
+if (number%i==0) 
+{ 
+while (number%i==0) 
+{ 
+printf ("%lld\t", i); 
+number/=i; 
+} 
+large_factor=i; 
+} 
+i++; 
+} 
+printf ("\n\nThe largest prime factor is : %lld\n\n", large_factor); 
+return 0; 
+} 
