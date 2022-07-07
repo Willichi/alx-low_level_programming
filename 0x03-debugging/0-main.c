fee288888cedc04bbcf489735a48ac31908a1ea7
willichi <onochiewilliam98@gmail.com>
2022-07-06 18:57:13 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+ *  main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 98;
+positive_or_negative(i);
 
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+return (0);
+}
