accd74bd0bf710e97091207a776ea629c6d84020
willichi <onochiewilliam98@gmail.com>
2022-07-08 02:51:55 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
-* print_line - prints an underscore n times
-* @n: number of times printed
+* print_line - entry point
 *
+* Description: Prints lines
+*@n: number of lines
 * Return: void
 */
-
-
 void print_line(int n)
 {
-int i;
-
-for (i = 0; i < n; i++)
+int num_lines;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (num_lines = 1; num_lines <= n; num_lines++)
 {
 _putchar('_');
 }
 _putchar('\n');
+}
 }
