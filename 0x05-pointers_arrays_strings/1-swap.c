d63fc7a79fc1cb81b10f0934dca376236de2f2f5
willichi <onochiewilliam98@gmail.com>
2022-07-11 00:43:12 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - check the code for Main files 
+* @a: variable.
+* @b: variable
+*/
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *b;
+*b = *a;
+*a = temp;
+}
